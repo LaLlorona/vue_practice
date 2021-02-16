46f943825b6651c8e607598dd096f8b29d878ad5
lallorona <chachaami66@gmail.com>
2021-02-16 03:19:35 +0000

    minor change

--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -12,39 +12,13 @@ export default {
     ListItem
   },
   created() {
-    // bus.$emit('start:spinner');
-    // setTimeout(() => {
-    //   this.$store.dispatch('FETCH_NEWS')
-    //   .then(() => {
-    //     console.log('current store is')
-    //     console.log(this.$store.news)
-    //     bus.$emit('end:spinner')
-        
-    //   })
-    //   .catch((err) => {
-    //     console.log(err);
-    //   })
-    // }, 3000)
-    // this.$store.dispatch('FETCH_NEWS')
-    // .then(() => console.log(this.$store.state.news))
-
-
-
-
-    //dispatch.then is not working well
+    //dispath.then work only when it returns promise 
     bus.$emit('start:spinner');
     this.$store.dispatch('FETCH_TOPSTORY')
     .then(() => {
       this.$store.dispatch('FETCH_NEW_TOPSTORY')
       .then(() => bus.$emit('end:spinner'))
     })
-    
-
-    // using settimeout works fine.
-    // this.$store.dispatch('FETCH_TOPSTORY');
-    // setTimeout(() => {
-    //   this.$store.dispatch('FETCH_NEW_TOPSTORY')
-    // }, 3000)
   },
   
 }
